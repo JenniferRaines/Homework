ab83b9fc3dcea074387d5c305402c93c30aaf0e9
Jennifer Raines <jennifer.laura.raines@gmail.com>
2015-04-06 21:06:23 -0600

    no message

---
+++ html_week_2/Stoplight/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by jenniferraines on 4/6/15.
+ */
+$(function(){
+
+    $('button').on('click',function(){
+        var color = $(this).attr('data-color');
+        console.log (color);
+
+    })
+
+
+
+
+
+
+
+
+
+
+});
